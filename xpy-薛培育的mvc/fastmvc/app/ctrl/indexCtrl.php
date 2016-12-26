2f5f723129ed575df231e4b49eb76542fadee1e1
xuepeiyu <309587263@qq.com>
2016-12-26 16:20:45 +0800

    xuepeiyu

---
+++ xpy-薛培育的mvc/fastmvc/app/ctrl/indexCtrl.php
@@ -0,0 +1,20 @@
+<?php 
+namespace app\ctrl;
+class indexCtrl  extends \core\run
+   {
+
+       public function index()
+       {
+             //P("this is index");
+             // $model = new \core\lib\model();
+             // $sql="select * from user";
+             // $arr=$model->query($sql);
+             // p($arr->fetchAll());
+           $data = "hello world";
+           $this->assign("data",$data);
+           $this->display('index/index.html');
+       }
+
+  }
+
+?>
